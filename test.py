8a9f2a858a020c480a1839a0a86a95f90d67fdaa
Barileva <veton@live.se>
2024-02-06 11:25:43 +0100

    f

--- test.py
+++ test.py
@@ -1,5 +1,23 @@
 
-# import numpy as np
+import numpy as np
+
+def omvandling_till_krypterat_alfabet(shift):
+    alfabet = np.array([chr(i) for i in range(97, 123)])
+    forskjutet_alfabet = np.roll(alfabet, -shift)
+    krypterat_alfabet = dict(zip(alfabet, forskjutet_alfabet))
+    return krypterat_alfabet
+
+def kryptering (meddelande, shift):
+    alfabetet = omvandling_till_krypterat_alfabet(shift)
+    krypterat_meddelande = "".join([alfabetet.get(i, i) for i in meddelande])
+    return krypterat_meddelande
+
+def dekryptering(meddelande, shift):
+    alfabetet = omvandling_till_krypterat_alfabet(-shift)
+    dekrypterat_meddelande = "".join([alfabetet.get(i, i) for i in meddelande])
+    return dekrypterat_meddelande
+
+
 
 def start_menu():
     while True:
@@ -9,16 +27,22 @@ def start_menu():
                     \n 2. dekryptera ord/meddelande
                     \n 3. se antalet krypterade ord/meddelande
                     \n 4. avsluta programmet
-                    """)
+                    \n Ange ditt val här: """)
+
+        shift = int(input("Ange antal steg för förskjutning i alfabetet (1-25)"))
+        meddelande = (input("Ange meddelandet du önskar kryptera?")).lower()
+        antalet_meddelande = 0
 
         if svar == "1":
-            shift = int(input("Ange antal steg för förskjutning i alfabetet"))
-
-            print("kryptera")
+            krypterat = kryptering (meddelande, shift)
+            antalet_meddelande += 1
+            print(f"krypterat meddelande: {krypterat}")
         elif svar == "2":
-            print("dekryptera")
+            dekrypterat = dekryptering(meddelande, shift)
+            antalet_meddelande -= 1
+            print(f"dekrypterat medelande: {dekrypterat}")
         elif svar == "3":
-            print("antalet meddelande")
+            print(f"Antal krypterade meddelande:  {antalet_meddelande}")
         elif svar == "4":
             break
         else:
