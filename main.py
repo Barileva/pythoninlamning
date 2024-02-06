a6793cf15763f5fe55ca559868603dfd50f34641
Barileva <veton@live.se>
2024-02-06 09:43:49 +0100

    ändringar

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
 
-# Press Skift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def start_menu():
+    while True:
+        svar = input("""
+                    välj ett av nedan alternativ 
+                    \n 1. kryptera  ord/meddelande 
+                    \n 2. dekryptera ord/meddelande
+                    \n 3. se antalet krypterade ord/meddelande
+                    \n 4. avsluta programmet
+                    """)
